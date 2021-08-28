6660cad01c53d75d138a0523aa0890598c7fab47
Rahul Dubey <rahuld7219@gmail.com>
2021-08-29 02:32:46 +0530

    Add a sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import Header from "../header/Header";
+import Posts from "../posts/Posts";
+import Sidebar from "../sidebar/Sidebar";
 import './home.css';
 
 export default function Home() {
     return (
-        <div className="home">
+        <>
             <Header />
-            homepage
-        </div>
+            <div className="home">
+                <Posts />
+                <Sidebar />
+            </div>
+        </>
     );
 }
