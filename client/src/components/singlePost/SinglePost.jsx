81b5210a3348d88030a86a399ea92517363f551c
Rahul Dubey <rahuld7219@gmail.com>
2021-09-14 15:12:15 +0530

    Display single post dynamically USED: react-router-dom dynamic link and axios to fetch single post

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,31 +1,41 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 import './singlePost.css';
 
 export default function SinglePost() {
+    const location = useLocation(); // gives information about the current URL
+    const postId = location.pathname.split("/")[2]; // useParams could also be used here instead
+    const [post, setPost] = useState({});
+    useEffect(() => {
+        const getPost = async () => {
+            const res = await axios.get(`/posts/${postId}`);
+            console.log(res);
+            setPost(res.data);
+        }
+        getPost();
+    }, []);
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img
+                {post.photo && (<img
                     className="singlePostImg"
                     src="https://ak.picdn.net/shutterstock/videos/32151379/thumb/4.jpg"
                     alt=""
-                />
+                />)}
                 <h1 className="singlePostTitle">
-                    Lorem ipsum dolor sit
+                    {post.title}
                     <div className="singlePostEdit">
                         <i className="singlePostIcon far fa-edit"></i>
                         <i className="singlePostIcon far fa-trash-alt"></i>
                     </div>
                 </h1>
                 <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author: <b>Rahul</b></span>
-                    <span className="singlePostDate">1 hour ago</span>
+                    <span className="singlePostAuthor">Author: <b>{post.username}</b></span>
+                    <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
                 <p className="singlePostDesc">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea eligendi est aperiam quod distinctio blanditiis tempore eum nisi nam sint amet, suscipit fuga voluptatum? Tempora atque iusto dolore suscipit culpa?
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea eligendi est aperiam quod distinctio blanditiis tempore eum nisi nam sint amet, suscipit fuga voluptatum? Tempora atque iusto dolore suscipit culpa?
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea eligendi est aperiam quod distinctio blanditiis tempore eum nisi nam sint amet, suscipit fuga voluptatum? Tempora atque iusto dolore suscipit culpa?
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea eligendi est aperiam quod distinctio blanditiis tempore eum nisi nam sint amet, suscipit fuga voluptatum? Tempora atque iusto dolore suscipit culpa?
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea eligendi est aperiam quod distinctio blanditiis tempore eum nisi nam sint amet, suscipit fuga voluptatum? Tempora atque iusto dolore suscipit culpa?
+                    {post.desc}
                 </p>
             </div>
         </div>
